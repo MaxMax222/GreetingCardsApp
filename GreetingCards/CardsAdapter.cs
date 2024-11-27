d6717c52daec5552902492962d8d84d21462e8b0
Maxim Zyuryaev <zrv.maxim@gmail.com>
2024-11-27 13:50:09 +0200

    added the option to watch the cards in the listview via dialog

--- GreetingCards/CardsAdapter.cs
+++ GreetingCards/CardsAdapter.cs
@@ -4,6 +4,8 @@ using Android.Views;
 using Android.Widget;
 using System.Collections.Generic;
 using Java.Lang;
+using Android.App;
+using Android.Graphics.Drawables;
 
 namespace GreetingCards
 {
@@ -11,6 +13,7 @@ namespace GreetingCards
 	{
         private readonly Context _context;
         private readonly List<GreetingCard> _items;
+        private Dialog dialog;
 		public CardsAdapter(Context context, List<GreetingCard> list)
 		{
             _context = context;
@@ -33,31 +36,71 @@ namespace GreetingCards
 
         public override View GetView(int position, View convertView, ViewGroup parent)
         {
-            var item = _items[position];
+            
+            var card = _items[position];
             var view = convertView;
             view ??= LayoutInflater.From(_context).Inflate(Resource.Layout.Item, parent, false);
 
             var icon = view.FindViewById<ImageView>(Resource.Id.cardImg);
             var sender = view.FindViewById<TextView>(Resource.Id.cardSender);
             var recipient = view.FindViewById<TextView>(Resource.Id.cardRecipient);
-
-            if (item is WeddingCard)
+            var btnDisplay = view.FindViewById<Button>(Resource.Id.display);
+            int imgId;
+            if (card is WeddingCard)
             {
-                icon.SetImageResource(Resource.Drawable.weddingCard);
+                imgId = Resource.Drawable.weddingCard;
+                icon.SetImageResource(imgId);
             }
-            else if(item is AdultBirthCard)
+            else if(card is AdultBirthCard)
             {
-                icon.SetImageResource(Resource.Drawable.adultBirthday);
+                imgId = Resource.Drawable.adultBirthday;
+                icon.SetImageResource(imgId);
             }
             else
             {
-                icon.SetImageResource(Resource.Drawable.youngBirthday);
+                imgId = Resource.Drawable.youngBirthday;
+                icon.SetImageResource(imgId);
             }
 
-            sender.Text = "Sender: " + item.Sender;
-            recipient.Text = "Recipient: " + item.Recipient;
+            sender.Text = "Sender: " + card.Sender;
+            recipient.Text = "Recipient: " + card.Recipient;
 
+            btnDisplay.Tag = position;
+            btnDisplay.Click += BtnDisplay_Click;
             return view;
+        }
+
+        private void BtnDisplay_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            int pos = (int)btn.Tag;
+            ShowCard(pos);
+        }
+
+        private void ShowCard(int pos)
+        {
+            dialog = new Dialog(_context);
+            dialog.SetCanceledOnTouchOutside(true);
+            dialog.SetContentView(Resource.Layout.showDialog);
+
+            var imgCard = dialog.FindViewById<ImageView>(Resource.Id.cardImg);
+            var card = _items[pos];
+            if (card is WeddingCard)
+            {
+                imgCard.SetImageResource(Resource.Drawable.weddingCard);
+            }
+            else if(card is AdultBirthCard)
+            {
+                imgCard.SetImageResource(Resource.Drawable.adultBirthday);
+            }
+            else
+            {
+                imgCard.SetImageResource(Resource.Drawable.youngBirthday);
+            }
+
+            var greeting = dialog.FindViewById<TextView>(Resource.Id.greeting);
+            greeting.Text = card.GreetingMSG();
+            dialog.Show();
         }
     }
 }
